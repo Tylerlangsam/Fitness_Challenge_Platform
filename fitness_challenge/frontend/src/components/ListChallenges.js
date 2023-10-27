974cdf903349acbc384166a3264c3639abfb5f48
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-10-26 20:50:58 -0700

    ListChallenges component

--- fitness_challenge/frontend/src/components/ListChallenges.js
+++ fitness_challenge/frontend/src/components/ListChallenges.js
@@ -1,1 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { listChallenges } from '../api';
+
+function ListChallenges() {
+    const [challenges, setChallenges] = useState([]);
+
+    useEffect(() => {
+        //Fetch the list of challenges from API
+
+        listChallenges().then((data) => setChallenges(data));
+    }, [])
+     return (
+    <div>
+      <h2>Challenges</h2>
+      <ul>
+        {challenges.map((challenge) => (
+          <li key={challenge.id}>{challenge.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListChallenges;
