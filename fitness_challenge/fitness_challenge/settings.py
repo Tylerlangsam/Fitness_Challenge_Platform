2781dfa9b53588d837020061bd4a8e8b8d2ccbfc
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-11-09 20:50:31 -0800

    tiredddd

--- fitness_challenge/fitness_challenge/settings.py
+++ fitness_challenge/fitness_challenge/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- fitness_challenge/frontend/src/components/AuthContext.jsx
+++ fitness_challenge/frontend/src/components/AuthContext.jsx
@@ -10,6 +10,18 @@ export function AuthProvider({ children }) {
     const [ user, setUser ] = useState(null);
 
     const login = async (userData) => {
-        try 
+        try {
+            const response = await axios.post('http://localhost:8000/api/auth/login/', userData)
+        
+            if (response.status === 200) {
+                setUser(response.userData);
+            }
+            else {
+                console.error("Login Failed");
+            }
+        }
+        catch(error){
+            console.error('Error during login:', error);
+        }
     }
 }
