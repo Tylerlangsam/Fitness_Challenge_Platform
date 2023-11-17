50e9ebe6fa8a68fbeb65fbe746742c0227aba749
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-11-16 19:28:49 -0800

    meh

--- fitness_challenge/frontend/src/App.js
+++ fitness_challenge/frontend/src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { BrowserRoter  as Router, Route, Swtich } from 'react-router-dom';
-import Home
 
 function App() {
-  return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-    </div>
-  );
+ const pass = []
 }
 
 export default App;
