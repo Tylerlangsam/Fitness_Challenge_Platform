a7eb5db7e5dba51bf6723dc2084e16d170a5cfc7
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-11-16 19:15:58 -0800

    User auth

--- fitness_challenge/frontend/src/components/AuthContext.jsx
+++ fitness_challenge/frontend/src/components/AuthContext.jsx
@@ -23,5 +23,45 @@ export function AuthProvider({ children }) {
         catch(error){
             console.error('Error during login:', error);
         }
+    };
+
+  const logout = async () => {
+    try {
+      // Perform additional cleanup during logout
+      await axios.post('http://localhost:8000/api/auth/logout/'); // Adjust the logout API endpoint
+
+      // Clear local storage or cookies containing authentication tokens
+      localStorage.removeItem('authToken'); // Adjust based on your token storage approach
+
+      // Remove any session-related data
+      sessionStorage.clear();
+
+      // Set the user state to null
+      setUser(null);
+    } catch (error) {
+      console.error('Error during logout:', error);
     }
+  };
+
+  const register = async (userData) => {
+    try {
+      const response = await axios.post('http://localhost:8000/api/auth/register/', userData);
+
+      if (response.status === 201) {
+        setUser(response.data);
+      }
+      else{
+        console.error('Registration Failed');
+    }
+    }
+    catch (error) {
+        console.error("error during registration", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, register }}>
+        {children}
+    </AuthContext.Provider>
+  )
 }
